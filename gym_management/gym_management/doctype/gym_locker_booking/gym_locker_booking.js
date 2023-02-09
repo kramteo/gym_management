c8f8811ed3d34903d11772704cf156d3674b4c96
kramteo <markteo17@gmail.com>
2023-02-09 08:10:01 +0000

    Locker Booking System completed

--- gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.js
+++ gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.js
@@ -8,6 +8,8 @@ frappe.ui.form.on('Gym Locker Booking', {
 		// console.log(var_time)
 		if (var_time > 23) {
 			var_time = 23;
+			let adjust_booking_hour = var_time - Number(frm.doc.start_time.slice(0,2));
+			frm.set_value('no_of_hours', adjust_booking_hour);
 		};
 		let var_end_time = var_time.toString() + ":00:00";
 		frm.set_value('start_time', var_start_time);
@@ -17,23 +19,44 @@ frappe.ui.form.on('Gym Locker Booking', {
 		let var_time = Number(frm.doc.start_time.slice(0,2)) + Number(frm.doc.no_of_hours)
 		if (var_time > 23) {
 			var_time = 23;
+			let adjust_booking_hour = var_time - Number(frm.doc.start_time.slice(0,2));
+			frm.set_value('no_of_hours', adjust_booking_hour);
 		};
 		let var_end_time = var_time.toString() + ":00:00";
 		frm.set_value('end_time', var_end_time);
 	},
-    validate : function(frm) {
-		let global_start_time2 = frappe.db.get_single_value("Gym Locker Details", "start_time")
-		let global_start_time = global_start_time2.then(toHour)
-		let global_end_time = frappe.db.get_single_value("Gym Locker Details", "end_time").then(toHour)
+    validate : async function(frm) {
+		let global_start_time = await frappe.db.get_single_value("Gym Locker Details", "start_time").then(toHour)
+		let global_end_time = await frappe.db.get_single_value("Gym Locker Details", "end_time").then(toHour)
 		let var_start_time = Number(frm.doc.start_time.slice(0,2))
 		let var_end_time = Number(frm.doc.end_time.slice(0,2))
 		let global_no_of_lockers = frappe.db.get_single_value("Gym Locker Details", "no_of_lockers")
 		console.log(global_start_time)
-		//console.log(var_start_time)
+		console.log(var_start_time)
 		if (var_start_time<global_start_time || var_end_time>global_end_time) {		
-			frappe.throw('You can not select past date in From Date' + global_start_time, global_end_time, global_no_of_lockers)
+			frappe.throw('You must select booking time between ' + global_start_time + ':00 and ' + global_end_time + ':00')
 		};
-	},
+		//if (gym_management.gym_management.doctype.gym_locker_booking.gym_locker_booking.validate_locker_availability(frm.doc.number) == false) {
+		//	console.log(frm.doc.number)
+		//};
+		let reply_message = await frappe.call({
+			method: "gym_management.gym_management.doctype.gym_locker_booking.gym_locker_booking.validate_locker_availability", //dotted path to server method
+			args: {
+				"booking_name":frm.doc.name,
+				"booking_date":frm.doc.date_of_booking,
+				"booking_start_time":frm.doc.start_time,
+				"booking_end_time":frm.doc.end_time,
+			},
+			callback: function(r) {
+				// code snippet
+				console.log(r.message)
+			}
+		});
+		console.log(typeof reply_message.message)
+		if (reply_message.message != true) {
+			frappe.throw('There is not enough lockers. Please choose another timing')
+		}
+	}
 });
 
 function toHour(strTime) {
--- gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
@@ -1,8 +1,37 @@
 # Copyright (c) 2023, MT and contributors
 # For license information, please see license.txt
 
-# import frappe
+from datetime import datetime # from python std library
+import frappe
 from frappe.model.document import Document
 
 class GymLockerBooking(Document):
 	pass
+
+@frappe.whitelist()
+def validate_locker_availability(booking_name,booking_date, booking_start_time, booking_end_time):
+	#frappe.msgprint(str(booking_date) + " start " + str(booking_start_time) + " end " + str(booking_end_time))
+	no_of_locker = int(frappe.db.get_single_value("Gym Locker Details", "no_of_lockers"))
+	start_hour = GetHour(booking_start_time)
+	end_hour = GetHour(booking_end_time)
+	booking_list = frappe.db.get_list("Gym Locker Booking", filters={'date_of_booking': booking_date}, fields=['start_time', 'end_time'])
+	count_booking = 0
+	for booking_item in booking_list:
+		if (booking_item.name != booking_name):
+			item_start_time = GetHour(str(booking_item.start_time))
+			item_end_time = GetHour(str(booking_item.end_time))
+			#frappe.msgprint(str(item_start_time) + " and " + str(item_end_time))
+			if ((item_start_time>=start_hour and item_start_time<=end_hour) or (item_end_time>=start_hour and item_end_time<=end_hour)):
+				count_booking +=1
+	#frappe.msgprint(str(count_booking))
+	if (count_booking<=no_of_locker):
+		ret_val = True
+	else:
+		ret_val = False
+	return ret_val
+
+def GetHour(time_str):
+	#frappe.msgprint(time_str)
+	num_str = time_str[0:time_str.find(":")]
+	num_int = int(num_str)
+	return num_int
